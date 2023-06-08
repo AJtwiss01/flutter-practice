10a0899c0edde52241d31818496181dc8a5be142
AJ Twiss <twissaj01@gmail.com>
2023-06-08 14:32:18 -0700

    finished chapter one

--- flutter-projects/chapter01/lib/main.dart
+++ flutter-projects/chapter01/lib/main.dart
@@ -1,62 +1,162 @@
 import 'package:flutter/material.dart';
+import 'util/utility.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    title: 'Measures Converter',
+    home: MyApp(),
+  ));
+}
 
 class MyApp extends StatefulWidget {
   @override
-  StatfullApp createState() => StatfullApp();
+  MyAppState createState() => MyAppState();
 }
 
-class StatfullApp extends State<MyApp> {
+class MyAppState extends State<MyApp> {
   double numberFrom = 0;
-
+  String startMeasure = '';
+  String convertedMeasure = '';
+  double result = 0;
+  String resultMessage = '';
   @override
-  void initState() {
-    numberFrom = 0;
-    super.initState();
-  }
-
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Measures Converter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Measures Converter'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              TextField(
-                onChanged: (text) {
-                  var rv = double.tryParse(text);
-                  if (rv != null) {
-                    setState(() {
-                      numberFrom = rv;
-                    });
-                  }
-                },
+    double sizeX = MediaQuery.of(context).size.width;
+    double sizeY = MediaQuery.of(context).size.height;
+    final TextStyle inputStyle = TextStyle(
+      fontSize: 20,
+      color: Colors.blue[900],
+    );
+    final TextStyle labelStyle = TextStyle(
+      fontSize: 24,
+      color: Colors.grey[700],
+    );
+
+    final spacer = Padding(padding: EdgeInsets.only(bottom: sizeY / 40));
+    final List<String> measures = [
+      'meters',
+      'kilometers',
+      'grams',
+      'kilograms',
+      'feet',
+      'miles',
+      'pounds (lbs)',
+      'ounces',
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Measures Converter'),
+      ),
+      body: Container(
+        width: sizeX,
+        padding: EdgeInsets.all(sizeX / 20),
+        child: SingleChildScrollView(
+            child: Column(
+          children: [
+            Text(
+              'Value',
+              style: labelStyle,
+            ),
+            spacer,
+            TextField(
+              style: inputStyle,
+              decoration: const InputDecoration(
+                hintText: "Please insert the measure to be converted",
               ),
-              Text((numberFrom == null) ? '' : numberFrom.toString())
-            ],
-          ),
-        ),
+              onChanged: (text) {
+                setState(() {
+                  numberFrom = double.parse(text);
+                });
+              },
+            ),
+            spacer,
+            Text(
+              'From',
+              style: labelStyle,
+            ),
+            spacer,
+            DropdownButton(
+              isExpanded: true,
+              style: inputStyle,
+              value: startMeasure,
+              items: measures.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(
+                    value,
+                    style: inputStyle,
+                  ),
+                );
+              }).toList(),
+              onChanged: (value) {
+                onStartMeasureChanged(value!);
+              },
+            ),
+            spacer,
+            Text(
+              'To',
+              style: labelStyle,
+            ),
+            spacer,
+            DropdownButton(
+              isExpanded: true,
+              style: inputStyle,
+              value: convertedMeasure,
+              items: measures.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(
+                    value,
+                    style: inputStyle,
+                  ),
+                );
+              }).toList(),
+              onChanged: (value) {
+                onConvertedMeasureChanged(value!);
+              },
+            ),
+            spacer,
+            ElevatedButton(
+              child: Text('Convert', style: inputStyle),
+              onPressed: () => convert(),
+            ),
+            spacer,
+            Text(
+              resultMessage,
+              style: labelStyle,
+            )
+          ],
+        )),
       ),
     );
   }
+
+  void onStartMeasureChanged(String value) {
+    setState(() {
+      startMeasure = value;
+    });
+  }
+
+  void onConvertedMeasureChanged(String value) {
+    setState(() {
+      convertedMeasure = value;
+    });
+  }
+
+  void convert() {
+    if (startMeasure.isEmpty || convertedMeasure.isEmpty || numberFrom == 0) {
+      return;
+    }
+    Conversion c = Conversion();
+    double result = c.convert(numberFrom, startMeasure, convertedMeasure);
+    setState(() {
+      result = result;
+      if (result == 0) {
+        resultMessage = 'This conversion cannot be performed';
+      } else {
+        resultMessage =
+            '${numberFrom.toString()} $startMeasure are ${result.toString()} $convertedMeasure';
+      }
+    });
+  }
 }
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Measures Converter',
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('Measures Converter'),
-//         ),
-//         body: Center(
-//           child: Text('Measures Converter'),
-//         ),
-//       ),
-//     );
-//   }
-// }
